cfc8436a8637b0361b38b31d27e45209c8b66c4d
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-14 15:35:14 -0400

    Added mean reversion strategy

---
+++ strategies/mean_reversion.py
@@ -0,0 +1,17 @@
+#Mean reversion:
+#buy when stock prices is significantly below rolling mean (z-score is below negative threshold)
+#sell when stock prices is significantly above rolling mean (z-score is above positive threshold)
+#z-score is number of standard deviations a value is from mean of distribution
+import pandas as pd
+
+def mean_reversion_strategy(df, window=20, z_score_threshold=1.5):
+    df['Rolling_Mean'] = df['Adj Close'].rolling(window=window).mean() #calculate rolling mean
+    df['Rolling_Std'] = df['Adj Close'].rolling(window=window).std() #calculate rolling standard deviation
+    
+    df['Z-Score'] = (df['Adj Close'] - df['Rolling_Mean']) / df['Rolling_Std']
+    
+    df['Signal'] = 0
+    df.loc[df['Z-Score'] < -z_score_threshold, 'Signal'] = 1 #buy signal
+    df.loc[df['Z-Score'] > z_score_threshold, 'Signal'] = -1 #sell signal
+    
+    return df
