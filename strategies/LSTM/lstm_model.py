aba128a0ee198a89bf11fcfb1dcafac6fb373a79
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-22 19:51:01 -0400

    Enhanced LSTM model with additional layers, batch normalization, and metrics for improved accuracy

--- strategies/LSTM/lstm_model.py
+++ strategies/LSTM/lstm_model.py
@@ -1,22 +1,38 @@
 #create LSTM model that will be trained on stock data
 import tensorflow as tf
 from tensorflow.keras.models import Sequential # type: ignore
-from tensorflow.keras.layers import Dense, LSTM, Dropout # type: ignore
+from tensorflow.keras.layers import Dense, LSTM, Dropout, BatchNormalization, TimeDistributed # type: ignore
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping # type: ignore
 
 def build_lstm_model(input_shape):
     model = Sequential()
     
-    #LSTM layer with 50 units
-    model.add(LSTM(units = 50, return_sequences=True, input_shape=input_shape))
-    model.add(Dropout(0.2))
     
-    model.add(LSTM(units = 50, return_sequences = False))
-    model.add(Dropout(0.2))
+    model.add(LSTM(units=128, return_sequences=True, input_shape=input_shape))
+    model.add(Dropout(0.3))
+    model.add(BatchNormalization())
+    
+    
+    model.add(LSTM(units=128, return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(BatchNormalization())
+    
+
+    model.add(LSTM(units=128, return_sequences=False))
+    model.add(Dropout(0.3))
+    model.add(BatchNormalization())
+    
+    
+    model.add(TimeDistributed(Dense(64, activation='relu')))
+    
     
     model.add(Dense(units=1))
     
-    model.compile(optimizer='adam', loss='mean_squared_error')
     
+    model.compile(optimizer='adam', 
+                loss='mean_squared_error', 
+                metrics=[tf.keras.metrics.MeanAbsoluteError(), 'mse'])
+
     return model
 
 #first layer has 50 units and returns sequences to be passed to next layer
