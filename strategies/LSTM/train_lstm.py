95005739dd85b7eedd9baecb2a9597481586c597
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-09-22 19:52:57 -0400

    Added callbacks, validation loss plotting, and scaler saving to training script

--- strategies/LSTM/train_lstm.py
+++ strategies/LSTM/train_lstm.py
@@ -2,18 +2,35 @@ import lstm_model
 import prepare_data
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping # type: ignore
 
 if __name__ == "__main__":
-    X_train, Y_train, scaler = prepare_data.prepare_data("../../data/NVDA_data.csv")
     
-    model = lstm_model.build_lstm_model((X_train.shape[1], 1))
+    X_train, X_val, Y_train, Y_val, scaler = prepare_data.prepare_data("../../data/NVDA_data.csv", features=['Open', 'High', 'Low', 'Adj Close'])
     
-    history = model.fit(X_train, Y_train, epochs=20, batch_size=32)
     
-    model.save("lstm_stock_model.h5") #saving trained model
+    model = lstm_model.build_lstm_model((X_train.shape[1], X_train.shape[2]))
     
-    plt.plot(history.history['loss'])
+    
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=0.00001)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    
+    
+    history = model.fit(X_train, Y_train, 
+                        epochs=50, 
+                        batch_size=32, 
+                        validation_data=(X_val, Y_val),
+                        callbacks=[reduce_lr, early_stopping])
+    
+
+    model.save("enhanced_lstm_stock_model.h5")
+    prepare_data.save_scaler(scaler, "scaler.save")
+    
+    
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
     plt.title('Model Loss During Training')
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
-    plt.show
+    plt.legend()
+    plt.show()
