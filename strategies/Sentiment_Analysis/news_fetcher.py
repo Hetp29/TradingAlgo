177baba049ac3439fee492b0fe4e366f564c2c35
Het Patel <hetpatel@Hets-MacBook-Air.local>
2024-09-26 15:06:10 -0400

    Loaded pre-trained FinBERT model for sentiment analysis

--- strategies/Sentiment_Analysis/news_fetcher.py
+++ strategies/Sentiment_Analysis/news_fetcher.py
@@ -4,12 +4,18 @@ from newsapi import NewsApiClient
 import datetime
 import os
 from dotenv import load_dotenv
+from transformers import BertTokenizer, BertForSequenceClassification, pipeline
 
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
 
 newsapi = NewsApiClient(api_key=api_key)
+
+tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone') #pre-trained finbert model for sentiment analysis 
+model = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone')
+
+finbert_sentiment = pipeline('sentiment analysis', model=model, tokenizer=tokenizer)
 
 def fetch_news(ticker, from_date=None, to_date=None):
     if not from_date:
@@ -27,10 +33,17 @@ def fetch_news(ticker, from_date=None, to_date=None):
     
     news_data = []
     for article in articles['articles']:
+        title = article['title']
+        description = article['description']
+        
+        sentiment = finbert_sentiment(title + " " + description if description else title)
+        sentiment_label = sentiment[0]['label']
+        
         news_data.append({
             'source': article['source']['name'],
             'title': article['title'],
             'description': article['description'],
+            'sentiment': sentiment_label,
             'url': article['url'],
             'published_at': article['publishedAt']
         })
